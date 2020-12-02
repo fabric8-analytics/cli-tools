08ef4312926716492b48d3246b1a4e622865aa22
dgpatelgit <dhpatel@redhat.com>
2020-12-02 16:01:53 +0530

    feat: [APPAI-1650] Take go executable path in the command (#10)
    * feat: [APPAI-1650] Take go executable path in the command
    
    * Optimized len() calls
    
    * Fixed review comments

--- gomanifest/build.go
+++ gomanifest/build.go
@@ -14,23 +14,35 @@ func main() {
 	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
 
 	// Validate required number of parameters.
-	if len(os.Args) != 3 {
+	argsLen := len(os.Args)
+	if argsLen != 3 && argsLen != 4 {
 		log.Error().Msg("invalid arguments for the command")
-		log.Info().Msgf("Usage :: %s <Path to source folder> <Output file path>/golist.json", os.Args[0])
-		log.Info().Msgf("Example :: %s /home/user/goproject/root/folder /home/user/gomanifest.json", os.Args[0])
+		log.Info().Msgf("Usage :: go run github.com/fabric8-analytics/cli-tools/gomanifest <Path to source folder> <Output file path>/golist.json [<Go executable path>]")
+		log.Info().Msgf("Example :: go run github.com/fabric8-analytics/cli-tools/gomanifest /home/user/goproject/root/folder /home/user/golist.json /usr/local/bin/go")
 		os.Exit(1)
 	}
 
 	// Ensure source path exists.
 	_, err := os.Stat(os.Args[1])
 	if err != nil {
-		log.Error().Err(err).Msg("invalid")
+		log.Error().Err(err).Msg("invalid folder")
 		os.Exit(2)
 	}
 
+	// Extract go executable path, if given
+	goExec := "go"
+	if argsLen == 4 {
+		_, err := os.Stat(os.Args[3])
+		if err != nil {
+			log.Error().Err(err).Msg("invalid executable")
+			os.Exit(3)
+		}
+		goExec = os.Args[3]
+	}
+
 	// Start generating manifest data.
-	log.Info().Msgf("Started analysing go project at %s", os.Args[1])
-	cmd, err := internal.RunGoList(os.Args[1])
+	log.Info().Msgf("Started analysing go project at [%s] using go exec [%s]", os.Args[1], goExec)
+	cmd, err := internal.RunGoList(goExec, os.Args[1])
 	if err != nil {
 		log.Error().Err(err).Msg("`go list` failed")
 		os.Exit(3)
@@ -39,7 +51,7 @@ func main() {
 	depPackages, err := internal.GetDeps(cmd)
 	if err != nil {
 		log.Error().Err(err).Msg("get deps")
-		os.Exit(3)
+		os.Exit(4)
 	}
 
 	manifest := internal.BuildManifest(&depPackages)
@@ -52,15 +64,15 @@ func main() {
 	// Create out file.
 	f, err := os.Create(os.Args[2])
 	if err != nil {
-		log.Error().Err(err).Msg("unable to write")
-		os.Exit(4)
+		log.Error().Err(err).Msg("unable to create")
+		os.Exit(5)
 	}
 	defer f.Close()
 
 	err = manifest.Write(f)
 	if err != nil {
 		log.Error().Err(err).Msg("unable to write")
-		os.Exit(5)
+		os.Exit(6)
 	}
 
 	log.Info().Msgf("Manifest file generated and stored at %s", os.Args[2])
