3202b674f34bf7757829512abfeddcba4e632d87
Deepak Sharma <deepshar@redhat.com>
2021-01-07 20:20:14 +0530

    pylist generator to go

--- analyses/pypi/pypi.go
+++ analyses/pypi/pypi.go
@@ -1,7 +1,7 @@
 package pypi
 
 import (
-	"fmt"
+	"io/ioutil"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -11,24 +11,18 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
-var pypiSupportedFormats = []string{
-	"*-requirements.txt",
-	"requirements-*.txt",
-	"requirements.txt",
-	"constraints-*.txt",
-	"*-constraints.txt",
-	"*-requirements.in",
-	"requirements-*.in"}
-
 // IsSupportedManifestFormat checks for Supported Formats
 func (*Matcher) IsSupportedManifestFormat(filename string) bool {
+	log.Debug().Msgf("Executing: IsSupportedManifestFormat")
 	s := strings.Split(filename, ".")
 	name, ext := s[0], s[1]
 	isExtSupported := checkExt(ext)
 	isNameSupported := checkName(name)
 	if isExtSupported && isNameSupported {
+		log.Debug().Msgf("Success: IsSupportedManifestFormat")
 		return true
 	}
+	log.Debug().Msgf("Success: IsSupportedManifestFormat")
 	return false
 }
 
@@ -36,7 +30,6 @@ func (*Matcher) IsSupportedManifestFormat(filename string) bool {
 func checkName(name string) bool {
 	match1, _ := regexp.MatchString("requirements?", name)
 	match2, _ := regexp.MatchString("constraints?", name)
-	fmt.Println(match1)
 	if match1 || match2 {
 		return true
 	}
@@ -57,9 +50,22 @@ func checkExt(ext string) bool {
 // GeneratorDependencyTree creates pylist.json from requirements.txt
 func (m *Matcher) GeneratorDependencyTree(manifestFilePath string) string {
 	log.Debug().Msgf("Executing: Generate Pylist")
+	m.getPylistGenerator()
 	pathToPylist := m.buildDepsTree("analyses/pypi/generate_pylist.py", manifestFilePath)
 	log.Debug().Msgf("Success: Generate Pylist")
 	return pathToPylist
+}
+
+// getPylistGenerator generates `generate_pylist.py from `generatepylist.go`
+func (m *Matcher) getPylistGenerator() error {
+	log.Debug().Msgf("Executing: getPylistGenerator")
+	// Generating generate_pylist.py
+	err := ioutil.WriteFile("analyses/pypi/generate_pylist.py", CodeForPylist, 0644)
+	if err != nil {
+		log.Fatal().Msg("Error Generating generate_pylist.py")
+	}
+	log.Debug().Msgf("Done: getPylistGenerator")
+	return nil
 }
 
 // buildDepsTree generates final Deps Tree and saves it as pylist.json
