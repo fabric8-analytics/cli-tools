6ef0892cea4f8030c06324b356491a4b7d10703f
Deepak Sharma <deepshar@redhat.com>
2021-01-11 19:02:12 +0530

    clearing files

--- cmd/analyse.go
+++ cmd/analyse.go
@@ -1,6 +1,8 @@
 package cmd
 
 import (
+	"os"
+
 	"github.com/fabric8-analytics/cli-tools/analyses/driver"
 	sa "github.com/fabric8-analytics/cli-tools/analyses/stackanalyses"
 	"github.com/rs/zerolog/log"
@@ -12,16 +14,40 @@ var manifestFile string
 
 // analyseCmd represents the analyse command
 var analyseCmd = &cobra.Command{
-	Use:   "analyse",
-	Short: "analyse performs Stack Analyses",
-	Long:  `analyse performs full Stack Analyses. Supported Ecosystems are Pypi, Maven, npm and golang.`,
-	Run:   runAnalyse,
+	Use:     "analyse",
+	Short:   "analyse performs Stack Analyses",
+	Long:    `analyse performs full Stack Analyses. Supported Ecosystems are Pypi, Maven, npm and golang.`,
+	Run:     runAnalyse,
+	PostRun: destructor,
 }
 
 func init() {
 	rootCmd.AddCommand(analyseCmd)
 	analyseCmd.PersistentFlags().StringVarP(&manifestFile, "file", "f", "", "Manifest file absolute path.")
 	analyseCmd.MarkPersistentFlagRequired("file")
+}
+
+// destructor deletes intermediatery files used to have stack analyses
+func destructor(cmd *cobra.Command, args []string) {
+	log.Debug().Msgf("Running Destructor.\n")
+	if debug {
+		// Keep intermediatery files, when on debug
+		return
+	}
+	intermediataryFiles := []string{"generate_pylist.py", "pylist.json", "dependencies.txt", "golist.json", "npmlist.json"}
+	for _, file := range intermediataryFiles {
+		if _, err := os.Stat(file); err != nil {
+			if os.IsNotExist(err) {
+				// If file doesn't exists, continue
+				continue
+			}
+		}
+		e := os.Remove(file)
+		if e != nil {
+			log.Fatal().Msgf("Error clearing files %s", file)
+		}
+	}
+	log.Debug().Msgf("Completed Destructor.\n")
 }
 
 //runAnalyse is controller func for analyses cmd.
