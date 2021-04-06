53f7c7f66f9ba0502b875d1722a22e7ff4c0672c
Deepak Sharma <deepshar@redhat.com>
2021-04-06 14:37:14 +0530

    feat: relative manifest file path support

--- cmd/validators.go
+++ cmd/validators.go
@@ -4,25 +4,16 @@ import (
 	"errors"
 	"fmt"
 	"os"
-	"path/filepath"
 
 	"github.com/spf13/cobra"
 )
 
-func checkFileExists(file string) bool {
-	isAbsPath := filepath.IsAbs(file)
-	if _, err := os.Stat(file); os.IsNotExist(err) {
-		return false
-	}
-	return isAbsPath
-}
-
 func validateFileArg(cmd *cobra.Command, args []string) error {
 	if len(args) < 1 {
-		return errors.New("requires absolute file path	")
+		return errors.New("Requires valid manifest file path.")
 	}
-	if checkFileExists(args[0]) {
-		return nil
+	if _, err := os.Stat(args[0]); os.IsNotExist(err) {
+		return errors.New(fmt.Sprintf("Invalid file path: %s", args[0]))
 	}
-	return fmt.Errorf("Invalid file path: %s", args[0])
+	return nil
 }
