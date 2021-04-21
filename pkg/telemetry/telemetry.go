0b05e7c28ae1ee868375372920f19a9682b32a7a
Deepak Sharma <deepshar@redhat.com>
2021-04-21 12:52:48 +0530

    test commit

--- pkg/telemetry/telemetry.go
+++ pkg/telemetry/telemetry.go
@@ -142,3 +142,8 @@ func SetVulnerability(ctx context.Context, value int) {
 func SetSnykTokenAssociation(ctx context.Context, value bool) {
 	setContextProperty(ctx, "snyk-token-associated", value)
 }
+
+// SetCrdaKey sets crda key
+func SetCrdaKey(ctx context.Context, value string) {
+	setContextProperty(ctx, "crda_key", value)
+}
